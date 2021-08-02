caa01c160426b4b98858a8a237640ea5641dd3b7
Kamlesh480 <kamleshchhipa480@gmail.com>
2021-08-02 18:34:45 +0530

    Initial commit from gatsby: (https://github.com/prakhar-goel/react-js-player.git)

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ * kamlesh chhipa
+ *
+ * See: https://www.gatsbyjs.com/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
